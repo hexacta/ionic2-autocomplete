bceaf9bedcfaa3e190bc8097b9dfcf51d6728584
icenteno <icenteno@hexacta.com>
2018-04-27 17:52:36 -0300

    Fix.

--- dev/src/autocomplete.component.ts
+++ dev/src/autocomplete.component.ts
@@ -63,7 +63,7 @@ const defaultOpts = {
             <span [innerHTML]='attrs.label | boldprefix:attrs.keyword'></span>
         </ng-template>
         <ul *ngIf="!disabled && suggestions.length > 0 && showList">
-            <li *ngFor="let suggestion of suggestions" (click)="select(suggestion);$event.stopPropagation()"">
+            <li *ngFor="let suggestion of suggestions" (click)="select(suggestion);$event.stopPropagation()">
                 <ng-template
                         [ngTemplateOutlet]="template || defaultTemplate"
                         [ngTemplateOutletContext]="
